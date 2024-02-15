e6f6d828e6b69a6e3be4503eba760ff0a633b37f
Blankaert <blankaertlouis@outlook.com>
2024-02-15 15:06:55 +0100

    User modification

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-const User = require("../models/users");
+const User = require("../models/User");
 const { checkBody } = require("../modules/checkBody");
 const uid2 = require("uid2");
 const bcrypt = require("bcrypt");
