6fb830cb356b985cb04ab926d7669721e323a638
Blankaert <blankaertlouis@outlook.com>
2024-02-15 17:28:51 +0100

    terminer les routes post get et delete

--- models/User.js
+++ models/User.js
@@ -35,6 +35,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("users", userSchema);
 
 module.exports = User;
--- routes/tweet.js
+++ routes/tweet.js
@@ -3,13 +3,48 @@ var router = express.Router();
 
 const Tweet = require("../models/Tweet");
 
-router.post("/twitter", async (req, res) => {
+router.post("/tweet", (req, res) => {
   const newTweet = new Tweet({
     content: req.body.content,
     author: req.body.author,
     hashtags: req.body.hashtags,
-    likes: req.body.likes,
   });
+  newTweet
+    .save()
+    .then(() => {
+      res.json({ result: true });
+    })
+    .catch((error) => {
+      console.error("Error saving tweet:", error);
+      res.status(500).json({ result: false, error: "Failed to save tweet" });
+    });
+});
+
+router.get("/tweets", async (req, res) => {
+  const tweets = await Tweet.find().populate("content").populate("hashtags");
+  res.json(tweets);
+});
+
+//delete
+router.delete("/tweet/:id", async (req, res) => {
+  try {
+    // Récupère l'ID du tweet à supprimer depuis les paramètres de l'URL
+    const tweetId = req.params.id;
+
+    // Supprime le tweet spécifique en utilisant son ID
+    const result = await Tweet.findByIdAndDelete(tweetId);
+
+    // Si aucun tweet n'est trouvé avec cet ID, renvoie une réponse indiquant que l'élément est introuvable
+    if (!result) {
+      return res.status(404).json({ message: "Tweet not found" });
+    }
+
+    // Envoie une réponse indiquant que la suppression a été réussie
+    res.json({ message: "Tweet successfully deleted" });
+  } catch (error) {
+    // Envoie une réponse en cas d'erreur
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
