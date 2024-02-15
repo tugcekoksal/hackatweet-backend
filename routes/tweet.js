bf59bb57d65c64afcb8cdc7dacdf9772303d50bb
Blankaert <blankaertlouis@outlook.com>
2024-02-15 16:29:28 +0100

    routes post

--- routes/tweet.js
+++ routes/tweet.js
@@ -3,4 +3,13 @@ var router = express.Router();
 
 const Tweet = require("../models/Tweet");
 
+router.post("/twitter", async (req, res) => {
+  const newTweet = new Tweet({
+    content: req.body.content,
+    author: req.body.author,
+    hashtags: req.body.hashtags,
+    likes: req.body.likes,
+  });
+});
+
 module.exports = router;
